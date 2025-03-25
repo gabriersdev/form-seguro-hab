c214f52f691b66821d7fde8bdcb9f964f4a4e407
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-03-25 14:55:04 -0300

    feat: implementando alteracoes para o form

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,39 +9,6 @@ import {
     console.log(e);
   }
   
-  // Apresentação do Projeto no console
-  const dados_do_projeto = {
-    'Project name': 'Ateste de Processo',
-    'Developed by': 'Gabriel Ribeiro',
-    Version: '1.0.1',
-    'Release date': '2024-05-04',
-    Hostname: new URL(window.location).hostname,
-    Origin: new URL(window.location).origin,
-    Status: 'Active',
-  };
-  
-  const novas_funcionalidades = [
-    'Recebimento de dados via parâmetro: dados enviados por URL (GET) são recuperados e lançados para criação do ateste do processo.',
-    'Formatação da capa para impressão: na impressão da capa, os campos são devidamente tratados e exibido apenas o conteúdo necessário.',
-    'Adicionado tratamento com regex para os dados recebidos via URL: os dados recebidos via URL são tratados com regex para garantir que apenas os caracteres necessários sejam exibidos.',
-    'Impressão do ateste quando os dados são recebidos via URL: ao receber os dados via URL, o ateste é impresso automaticamente.',
-  ];
-  
-  Object.freeze(novas_funcionalidades);
-  Object.freeze(dados_do_projeto);
-  
-  // Exibindo dados
-  console.groupCollapsed(`${dados_do_projeto['Project name']}, Version ${dados_do_projeto.Version}`);
-  console.table(dados_do_projeto);
-  console.groupEnd();
-  
-  console.groupCollapsed('New features');
-  novas_funcionalidades.toSorted((a, b) => a.localeCompare(b)).forEach((feature) => {
-    console.info(`${feature}`);
-  });
-  console.groupEnd();
-  // Fim da apresentação do projeto
-  
   document.querySelectorAll('[data-recarrega-pagina]').forEach((botao) => {
     botao.addEventListener('click', () => {
       window.location.reload();
@@ -75,7 +42,7 @@ import {
           break;
         
         case 'github-projeto':
-          link.href = 'https://github.com/gabriersdev/ateste-processo';
+          link.href = 'https://github.com/gabriersdev/form-seguro-hab';
           break;
       }
       
@@ -92,119 +59,9 @@ import {
   
   // TODO - refazer com os campos do formulário de seguro
   function enviarFormulario() {
-    const inputs_tratamento = ['cc_agencia', 'cc_operacao', 'cc_numero', 'cc_digito', 'cp_agencia', 'cp_operacao', 'cp_numero', 'cp_digito'];
-    
-    const contas = {
-      corrente: [
-        document.querySelector('#conta_corrente').checked,
-        !isEmpty(document.querySelector('#cc_agencia').value),
-        !isEmpty(document.querySelector('#cc_operacao').value),
-        !isEmpty(document.querySelector('#cc_numero').value),
-      ],
-      poupanca: [
-        document.querySelector('#conta_poupanca').checked,
-        !isEmpty(document.querySelector('#cp_agencia').value),
-        !isEmpty(document.querySelector('#cp_operacao').value),
-        !isEmpty(document.querySelector('#cp_numero').value),
-      ],
-    };
-    
-    if (!contas.corrente.every((e) => e)) {
-      $('[data-input="cc_agencia"]').val('');
-      $('[data-input="cc_operacao"]').val('');
-      $('[data-input="cc_numero"]').val('');
-      $('sxs[refer="cc_digito"]').text('');
-      
-      if (contas.corrente.some((e) => e) && !document.querySelector('[data-e="dados-bancarios-conta-corrente"] [role="alert"]')) $('[data-e="dados-bancarios-conta-corrente"]').prepend('<div role="alert"><div class="alert alert-danger"><p>Um ou mais campos não foram preenchidos!</p></div></div>');
-    } else {
-      $('[data-e="dados-bancarios-conta-corrente"] [role="alert"]').remove();
-    }
-    
-    if (!contas.poupanca.every((e) => e)) {
-      $('[data-input="cp_agencia"]').val('');
-      $('[data-input="cp_operacao"]').val('');
-      $('[data-input="cp_numero"]').val('');
-      $('sxs[refer="cp_digito"]').text('');
-      
-      if (contas.poupanca.some((e) => e) && !document.querySelector('[data-e="dados-bancarios-conta-poupanca"] [role="alert"]')) $('[data-e="dados-bancarios-conta-poupanca"]').prepend('<div role="alert"><div class="alert alert-danger"><p>Um ou mais campos não foram preenchidos!</p></div></div>');
-    } else {
-      $('[data-e="dados-bancarios-conta-poupanca"] [role="alert"]').remove();
-    }
-    
-    $('[data-input]').each((index, element) => {
-      if (element.tagName.toLowerCase() === 'input') {
-        const tipo = element.type;
-        const area = $(`sxs[refer=${element.dataset.input}]`);
-        
-        if (!isEmpty(tipo) && !isEmpty(area)) {
-          switch (tipo) {
-            case 'text':
-              
-              if (!isEmpty(element.value)) {
-                area.text(`${element.value.toUpperCase().trim()}`);
-              } else {
-                area.text('');
-              }
-              
-              if (inputs_tratamento.includes(element.dataset.input)) {
-                switch (element.dataset.input) {
-                  case 'cc_agencia':
-                  case 'cp_agencia':
-                    if (element.value.length <= 5) area.html(replicar(5, element.value, '&emsp;'));
-                    if (element.value.length === 0) area.html('&emsp;&emsp;&emsp;');
-                    break;
-                  
-                  case 'cc_operacao':
-                  case 'cp_operacao':
-                    if (element.value.length <= 4) area.html(replicar(5, element.value, '&emsp;'));
-                    if (element.value.length === 0) area.html('&emsp;&emsp;&emsp;');
-                    break;
-                  
-                  case 'cc_numero':
-                  case 'cp_numero':
-                    
-                    const valor = element.value.replaceAll('-', '');
-                    if (element.value.length <= 14) area.html(replicar(14, numero_e_digito(valor).numero, '&emsp;'));
-                    if (element.value.length === 0) area.html('&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;');
-                    
-                    if (element.dataset.input === 'cc_numero') {
-                      $('sxs[refer="cc_digito"]').text(numero_e_digito(valor).digito);
-                    } else if (element.dataset.input === 'cp_numero') {
-                      $('sxs[refer="cp_digito"]').text(numero_e_digito(valor).digito);
-                    }
-                    break;
-                }
-              }
-              break;
-            
-            case 'date':
-              const data = element.value;
-              try {
-                if (data.length === 10) {
-                  const split = data.split('-');
-                  $('sxs[refer="data_ano"]').text(split[0].trim());
-                  $('sxs[refer="data_dia"]').text(split[2].trim());
-                  
-                  if (split[1][0] === '0') {
-                    $('sxs[refer="data_mes_extenso"]').text((converterParaMesBRL(parseInt(split[1][1], 10) - 1)).toUpperCase());
-                  } else {
-                    $('sxs[refer="data_mes_extenso"]').text((converterParaMesBRL(parseInt(split[1], 10) - 1)).toUpperCase());
-                  }
-                }
-              } catch (error) {
-                console.log('Um erro ocorreu. Erro: %s', error);
-              }
-              break;
-            
-            case 'checkbox':
-            case 'radio':
-              if (element.checked) area.text('X');
-              else area.text('');
-              break;
-          }
-        }
-      }
-    });
+    const inputs_tratamento = ['cc_agencia', 'cc_operacao', 'cc_numero', 'cc_digito', 'cp_agencia', 'cp_operacao', 'cp_numero', 'cp_digito']; /* incluir CPFs?  */
+    console.log('XXXX')
+    // TODO - dar o tratamento e fazer o preenchimento dos dados
   }
   
   function atribuirAcoes() {
@@ -241,19 +98,6 @@ import {
             event.preventDefault();
             enviarFormulario();
             acao.closest('dialog').close();
-          });
-          break;
-        
-        case 'toggle-dados-bancarios':
-          $(acao).on('input', () => {
-            if (acao.checked) {
-              $((acao.closest('[data-content="dados-conta"]')).querySelector('[data-content="dados-bancarios"]')).show(300);
-              setTimeout(() => {
-                (acao.closest('[data-content="dados-conta"]')).querySelector('[data-content="dados-bancarios"]').querySelectorAll('input')[0].focus();
-              }, 500);
-            } else {
-              $((acao.closest('[data-content="dados-conta"]')).querySelector('[data-content="dados-bancarios"]')).hide(300);
-            }
           });
           break;
         
@@ -297,22 +141,6 @@ import {
           
           case 'conta':
             $(input).mask('000000000000-0', {reverse: true});
-            break;
-          
-          case 'conta-vendedor':
-            $(input).mask('000000000000-0', {reverse: true});
-            break;
-          
-          case 'money':
-            SimpleMaskMoney.setMask(input, {
-              prefix: 'R$ ',
-              fixed: true,
-              fractionDigits: 2,
-              decimalSeparator: ',',
-              thousandsSeparator: '.',
-              cursor: 'end',
-            });
-            input.removeAttribute('maxlength');
             break;
           
           default:
@@ -383,8 +211,9 @@ import {
     
     try {
       const url = new URLSearchParams(new URL(window.location).search);
-      const parametros_insercao = new Array();
-      const modalidades = ['CCNPMCMV', 'CCFGTS', 'CCSBPE', 'PROCOTISTA'];
+      const parametros_insercao = [];
+      
+      console.log(url)
       
       document.querySelectorAll('sxs[refer]').forEach((sxs) => {
         parametros_insercao.push(sxs.getAttribute('refer'));
@@ -401,7 +230,7 @@ import {
             const elemento = document.querySelector(`[data-input=${parametro}]`);
             const {type} = elemento;
             
-            const parametros_para_tratar = ['CPF_1', 'CPF_2', 'n_contrato', 'cc_agencia', 'cc_operacao', 'cc_numero', 'cp_agencia', 'cp_operacao', 'cp_numero'];
+            const parametros_para_tratar = ['CPF_1', 'CPF_2', 'CPF_3', 'CPF_4', 'n_contrato', 'cc_agencia', 'cc_operacao', 'cc_numero', 'cp_agencia', 'cp_operacao', 'cp_numero'];
             
             switch (type) {
               case 'text':
@@ -409,6 +238,8 @@ import {
                   switch (parametro) {
                     case 'CPF_1':
                     case 'CPF_2':
+                    case 'CPF_3':
+                    case 'CPF_4':
                       elemento.value = url.get(parametro).replace(/\D/g, '').substring(0, 11) || '';
                       atribuirMascaras('cpf', elemento);
                       if (verificarCPF(elemento.value)) {
@@ -468,11 +299,6 @@ import {
         setTimeout(() => {
           document.querySelector('.btn-impressao').click();
         }, 500);
-      }
-      
-      if (!isEmpty(url.has('modalidade')) && modalidades.includes(url.get('modalidade'))) {
-        $(`sxs[refer=${url.get('modalidade')}]`).text('X');
-        document.querySelector(`#${url.get('modalidade')}`).checked = true;
       }
     } catch (error) {
       console.log('Ocorreu um erro ao tentar recuperar os dados da URL. Erro: %s', error);
